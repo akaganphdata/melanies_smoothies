90a4359401ae44cc811b42e54d6248a346cd3550
akaganphdata <akagan@phdata.io>
2025-08-17 17:16:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,16 +29,10 @@ if ingredients_list:
     ingredients_string = ' '.join(ingredients_list)
 
 time_to_insert = st.button('Submit Order')
-
 if time_to_insert and ingredients_string and name_on_order:
-    df_to_insert = pd.DataFrame({
-        "INGREDIENTS": [ingredients_string],
-        "NAME_ON_ORDER": [name_on_order]
-    })
-
-    # Convert to Snowpark DataFrame
-    order_df = session.create_dataframe(df_to_insert)
-
-    # Append to table
-    order_df.write.mode("append").save_as_table("smoothies.public.orders")
+    insert_stmt = """
+        INSERT INTO smoothies.public.orders (INGREDIENTS, NAME_ON_ORDER)
+        VALUES (%s, %s)
+    """
+    session._conn.cursor().execute(insert_stmt, (ingredients_string, name_on_order))
     st.success(f'Your smoothie is ordered, {name_on_order}!', icon="✅")
